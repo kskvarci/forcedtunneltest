05fab71ddab789f68ce9bfc961cda3d2b660027e
Ken Skvarcius <kskvarci@gmail.com>
2019-09-02 13:57:43 -0400

    update vnet name

--- scripts/deployjumpbox.sh
+++ scripts/deployjumpbox.sh
@@ -3,9 +3,9 @@
 # Parameters
 location="eastus2"
 resourceGroupName="TestNet-Rg"
-hubVnetName="TestVnet"
-userName="your username here"
-sshKey="your public ssh key here"
+hubVnetName="HubVnet"
+userName="username"
+sshKey="sshkey"
 
 # Deploy an Ubuntu VM and install Squid Proxy
 az vm create --name "Jumpbox" --resource-group $resourceGroupName --authentication-type "ssh" --admin-username $userName --boot-diagnostics-storage "" --location $location --nsg "" --image "Canonical:UbuntuServer:18.04-LTS:latest" --size "Standard_DS2" --ssh-key-value "$sshKey" --subnet "jumpbox-subnet" --vnet-name $hubVnetName
