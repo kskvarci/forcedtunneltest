1f5d9fcd115cf364d8a40d9406db09b6ab081fcf
Ken <git@proc.run>
2020-02-18 12:38:36 -0500

    Fix

--- scripts/01-deployVnet.sh
+++ scripts/01-deployVnet.sh
@@ -2,12 +2,12 @@
 
 # Parameters
 location="eastus2"
-resourceGroupName="AKS-Workshop-Rg"
+resourceGroupName="HubSpoke-Rg"
 
 spokeVnetName="SpokeVnet"
 hubVnetName="HubVnet"
 
-sourceIP="xxx.xxx.xxx.xxx/32"
+sourceIP="65.190.18.112/32"
 
 # Create a resource group
 az group create --location $location --name $resourceGroupName
@@ -52,7 +52,7 @@ az network vnet subnet update --resource-group $resourceGroupName --name "worklo
 az network vnet subnet update --resource-group $resourceGroupName --name "proxy-subnet" --vnet-name $hubVnetName --network-security-group "proxy-nsg"
 az network vnet subnet update --resource-group $resourceGroupName --name "jumpbox-subnet" --vnet-name $hubVnetName --network-security-group "jumpbox-nsg"
 
-# Create a user-defined route to drop internet detined traffic
+# Create a user-defined route to drop internet destined traffic
 az network route-table create --resource-group $resourceGroupName --name "workload-udr"
 az network route-table route create --resource-group $resourceGroupName --name "dropinternet-route" --route-table-name "workload-udr" --address-prefix "0.0.0.0/0" --next-hop-type "None"
 
